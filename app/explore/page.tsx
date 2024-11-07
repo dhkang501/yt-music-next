780406cbd55bd4281242bb7982cc68b8d3e42a97
dhkang501 <kangg5@naver.com>
2024-11-07 14:15:27 +0900

    - layout에 header 추가 - explore page에 PlayListCarousel 컴포넌트 추가 - PlayListCarousel 반응형별 카드노출 개수 수정

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Catagory from '@/app/explore/components/Category';
 import PagePadding from '@/components/PagePadding';
+import {getAllPlaylist} from '@/lib/dummyData';
+import PlayListCarousel from '@/components/PlayListCarousel';
 
-const page = () => {
+const page = async () => {
+  const playlistArray = await getAllPlaylist();
   return (
     <PagePadding>
       <div className="mt-4"></div>
       <Catagory />
+      <PlayListCarousel playlistArray={playlistArray} title="새 앨범 및 싱글" />
     </PagePadding>
   );
 };
