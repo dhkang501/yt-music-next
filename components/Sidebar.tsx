0ffe8a78284e70b20a6d5af16627e0e11214fbff
dhkang501 <kangg5@naver.com>
2024-11-11 16:41:55 +0900

    - 인기곡 아이콘 및 이미지 추가 - 사이드바 css 수정

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,26 +1,26 @@
-import React, { ReactNode } from 'react'
-import Logo from '@/components/elements/Logo'
-import Navigator from '@/components/elements/Navigator'
+import React, {ReactNode} from 'react';
+import Logo from '@/components/elements/Logo';
+import Navigator from '@/components/elements/Navigator';
 
 interface SidebarProps {
-    children: ReactNode;
-  }
+  children: ReactNode;
+}
 
 const Sidebar: React.FC<SidebarProps> = ({children}) => {
   return (
-    <div className='flex h-full'>
-        <nav className='w-[240px] hidden lg:block border-r-[1px] border-netural-600'>
-            <div className='p-[24px]'>
-                <Logo/>
-            </div>
-            <div>
-                <Navigator/>
-            </div>
-        </nav>
-        {/* flex-1로 너비값 100% 사용할 수 있음 */}
-        <div className='flex-1 min-h-[600px]'>{children}</div>
+    <div className="flex h-full">
+      <nav className="w-[240px] hidden lg:block border-r-[1px] border-netural-600">
+        <div className="p-[24px]">
+          <Logo />
+        </div>
+        <div>
+          <Navigator />
+        </div>
+      </nav>
+      {/* flex-1로 너비값 100% 사용할 수 있음 */}
+      <div className="w-full lg:w-[calc(100%-240px)]">{children}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
