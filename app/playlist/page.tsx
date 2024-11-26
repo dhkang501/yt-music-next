a4fc71cab147ebc17068029d2c1f15cddf703f20
dhkang501 <kangg5@naver.com>
2024-11-26 14:51:14 +0900

    playlist SongList 추가

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,29 +1,36 @@
-import {getPlaylistById} from '@/lib/dummyData'
-import {getRandomElementArray} from '@/lib/utils'
-import {permanentRedirect} from 'next/navigation'
-import React from 'react'
-import HeaderBgChanger from '@/components/HeaderBgChanger'
-import PlayListHead from '@/components/PlayListHead'
-import PagePadding from '@/components/PagePadding'
+import {getPlaylistById} from '@/lib/dummyData';
+import {getRandomElementArray} from '@/lib/utils';
+import {permanentRedirect} from 'next/navigation';
+import React from 'react';
+import HeaderBgChanger from '@/components/HeaderBgChanger';
+import PlayListHead from '@/components/PlayListHead';
+import PagePadding from '@/components/PagePadding';
+import SongCardRowExpand from '@/components/SongCardRowExpand';
 
 interface PlaylistPageProps {
   searchParams: {
-    list: string
-  }
+    list: string;
+  };
 }
 
 const page = async (props: PlaylistPageProps) => {
-  const playlist = await getPlaylistById(Number(props.searchParams.list))
-  if (!playlist) permanentRedirect('/')
-  const imageSrc = getRandomElementArray(playlist.songList)?.imageSrc
+  const playlist = await getPlaylistById(Number(props.searchParams.list));
+  if (!playlist) permanentRedirect('/');
+  const imageSrc = getRandomElementArray(playlist.songList)?.imageSrc;
 
   return (
     <PagePadding>
       <HeaderBgChanger imageSrc={imageSrc} />
       <div className="mt-12"></div>
       <PlayListHead playlist={playlist} />
+      <div className="mt-12"></div>
+      <section className="flex flex-col gap-2">
+        {playlist.songList.map((song, idx) => {
+          return <SongCardRowExpand song={song} />;
+        })}
+      </section>
     </PagePadding>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ components/SongCardRowExpand.tsx
@@ -0,0 +1,44 @@
+'use client';
+import Image from 'next/image';
+import React from 'react';
+import {FaCircle} from 'react-icons/fa';
+import {AiOutlineCaretDown} from 'react-icons/ai';
+import {AiOutlineCaretUp} from 'react-icons/ai';
+import {FiPlayCircle} from 'react-icons/fi';
+import {FiThumbsDown} from 'react-icons/fi';
+import {FiThumbsUp} from 'react-icons/fi';
+import {FiMoreVertical} from 'react-icons/fi';
+import IconButton from './elements/IconButton';
+import {Song} from '@/app/types';
+
+interface SongCardRowExpandProps {
+  song: Song;
+}
+
+const SongCardRowExpand: React.FC<SongCardRowExpandProps> = ({song}) => {
+  return (
+    <article
+      className="flex flex-row items-center gap-4 h-[48px] w-full
+        relative group">
+      <div className=" w-[48px] h-[48px] relative">
+        <Image src={song.imageSrc} alt="img" fill className=" object-cover" />
+        <section
+          className="hidden group-hover:flex absolute top-0 w-[48px] h-[48px] items-center justify-center bg-black
+        cursor-pointer
+        ">
+          <FiPlayCircle size={20} />
+        </section>
+      </div>
+      <div>
+        <div>{song.name}</div>
+      </div>
+      <section className="hidden group-hover:flex absolute top-0 right-0 flex-row justify-end items-center h-[48px] w-1/2 bg-[rgba(0,0,0,0.7)]">
+        <IconButton icon={<FiThumbsDown size={20} />} />
+        <IconButton icon={<FiThumbsUp size={20} />} />
+        <IconButton icon={<FiMoreVertical size={20} />} />
+      </section>
+    </article>
+  );
+};
+
+export default SongCardRowExpand;
