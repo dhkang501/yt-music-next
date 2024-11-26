41840949724019bc6f9b89e3f9697f2acaebd912
dhkang501 <kangg5@naver.com>
2024-11-26 15:20:07 +0900

    playlist - SongList router link 추가

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from 'react';
 
-const page = (props) => {
-    console.log(props.id)
-  return (
-    <div>channel{props.params.id}</div>
-  )
-}
+const page = props => {
+  console.log(props.id);
+  return <div>channel{props.params.id}</div>;
+};
 
-export default page
+export default page;
--- components/SongCardRowExpand.tsx
+++ components/SongCardRowExpand.tsx
@@ -10,12 +10,19 @@ import {FiThumbsUp} from 'react-icons/fi';
 import {FiMoreVertical} from 'react-icons/fi';
 import IconButton from './elements/IconButton';
 import {Song} from '@/app/types';
+import {useRouter} from 'next/navigation';
 
 interface SongCardRowExpandProps {
   song: Song;
 }
 
 const SongCardRowExpand: React.FC<SongCardRowExpandProps> = ({song}) => {
+  const {channel, channelId} = song;
+  const {push} = useRouter();
+
+  const onClickChannel = () => {
+    push(`/channel/${channelId}`);
+  };
   return (
     <article
       className="flex flex-row items-center gap-4 h-[48px] w-full
@@ -29,10 +36,15 @@ const SongCardRowExpand: React.FC<SongCardRowExpandProps> = ({song}) => {
           <FiPlayCircle size={20} />
         </section>
       </div>
-      <div>
-        <div>{song.name}</div>
+      <div className="flex flex-row gap-4 justify-between basis-1/3">
+        <div className="w-[130px] truncate">{song.name}</div>
+        <div
+          onClick={onClickChannel}
+          className="text-neutral-500 hover:underline cursor-pointer">
+          {channel}
+        </div>
       </div>
-      <section className="hidden group-hover:flex absolute top-0 right-0 flex-row justify-end items-center h-[48px] w-1/2 bg-[rgba(0,0,0,0.7)]">
+      <section className="hidden group-hover:flex absolute top-0 right-0 flex-row justify-end items-center h-[48px] w-[120px] bg-[rgba(0,0,0,0.7)]">
         <IconButton icon={<FiThumbsDown size={20} />} />
         <IconButton icon={<FiThumbsUp size={20} />} />
         <IconButton icon={<FiMoreVertical size={20} />} />
